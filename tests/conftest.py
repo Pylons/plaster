5f8392f5ee3b3a5d04646364da1ff6ed7a421a73
Michael Merickel <michael@merickel.org>
2017-03-26 16:40:53 -0500

    stop monkeypatching pkg_resources and just add the fixtures for the whole duration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
 import os.path
 import pkg_resources
 import pytest
+import sys
 
-@pytest.fixture
-def fake_packages(monkeypatch):
+@pytest.fixture(scope='session')
+def fake_packages():
+    # we'd like to keep this scope more focused but it's proven really
+    # difficult to fully monkeypatch pkg_resources and so for now we just
+    # install the packages for the duration of the test suite
     test_dir = os.path.dirname(__file__)
-    ws = pkg_resources.WorkingSet()
     for name in ('app1', 'app2'):
         info_dir = os.path.join(test_dir, 'fake_packages', name)
-        ws.add_entry(info_dir)
-        monkeypatch.syspath_prepend(info_dir)
-    monkeypatch.setattr('pkg_resources.working_set', ws)
-    monkeypatch.setattr('pkg_resources.iter_entry_points', ws.iter_entry_points)
-    yield ws
+        sys.path.insert(0, info_dir)
+        pkg_resources.working_set.add_entry(info_dir)
