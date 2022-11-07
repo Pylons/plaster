733b1e0751c6d0bdafadf0d041f006514464cd84
Michael Merickel <michael@merickel.org>
2022-11-06 19:45:02 -0600

    drop setuptools dependency for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,16 @@
 import os.path
 import sys
 
-import pkg_resources
 import pytest
 
 
 @pytest.fixture(scope="session")
 def fake_packages():
     # we'd like to keep this scope more focused but it's proven really
-    # difficult to fully monkeypatch pkg_resources and so for now we just
+    # difficult to fully monkeypatch importlib.metadata and so for now we just
     # install the packages for the duration of the test suite
     test_dir = os.path.dirname(__file__)
 
     for name in ("app1", "app2"):
         info_dir = os.path.join(test_dir, "fake_packages", name)
         sys.path.insert(0, info_dir)
-        pkg_resources.working_set.add_entry(info_dir)
